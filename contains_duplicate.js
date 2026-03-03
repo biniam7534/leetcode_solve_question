b854baf461c4a2f47fb4b5e4e9c0e088282dd4fe
biniam7534 <biniamabuu75@gmail.com>
2026-03-03 22:39:26 +0300

    to solve contains_duplicate problem

---
+++ contains_duplicate.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const uniqueNums = new Set(nums);
+    return uniqueNums.size !== nums.length;
+    
+}
+console.log(containsDuplicate([1,2,3,1])); 
+console.log(containsDuplicate([1,2,3,4])); 
+console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2])); 
